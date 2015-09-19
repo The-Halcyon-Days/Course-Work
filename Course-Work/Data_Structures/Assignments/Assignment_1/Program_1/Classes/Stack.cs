b082f57ca3b3bbff7988e68f20ccbbc0d7ee2994
Halcyon <vyandra@gmail.com>
2015-09-18 23:54:40 -0700

    Added the stack class

--- Course-Work/Data_Structures/Assignments/Assignment_1/Program_1/Classes/Stack.cs
+++ Course-Work/Data_Structures/Assignments/Assignment_1/Program_1/Classes/Stack.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,31 @@ namespace Program_1.Classes
 {
     class Stack
     {
+        private readonly static int[] StackArray = new int[10];
+        private static int _topOfStack = -1;
+
+        public static void Push(int element)
+        {
+            _topOfStack++;
+            StackArray[_topOfStack] = element;
+        }
+
+        public static int Pop()
+        {
+            int poppedElement = StackArray[_topOfStack];
+            _topOfStack--;
+            return poppedElement;
+        }
+
+        public static int Peek()
+        {
+            return StackArray[_topOfStack];
+        }
+
+        public static bool IsEmpty()
+        {
+            return _topOfStack == -1 ? true : false;
+        }
+
     }
 }

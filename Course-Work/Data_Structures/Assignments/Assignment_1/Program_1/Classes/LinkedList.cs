50dd7f4914ee4b60d119e155517384426ad110dc
Halcyon <vyandra@gmail.com>
2015-09-20 23:42:47 -0700

    Added some things to Linked List assignment

--- Course-Work/Data_Structures/Assignments/Assignment_1/Program_1/Classes/LinkedList.cs
+++ Course-Work/Data_Structures/Assignments/Assignment_1/Program_1/Classes/LinkedList.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,55 @@ namespace Program_1.Classes
 {
     class LinkedList
     {
+        class Node
+        {
+            public Node Link;
+            public int Value;
+        }
+
+
+        private Node _head = null;
+
+        public void Add(int element)
+        {
+            Node newNode = new Node
+            {
+                Link = _head,
+                Value = element
+            };
+            _head = newNode;
+        }
+
+        public int RemoveLast()
+        {
+            Node currNode = _head;
+            Node tempNode = null;
+
+            while (currNode != null)
+            {
+                Console.WriteLine(currNode.Value);
+                if (currNode.Link == _head)
+                {
+                    tempNode = _head;
+                    _head = currNode;
+                    break;
+                }
+                currNode = currNode.Link;
+            }
+            return tempNode.Value;
+        }
+
+        public void Remove(int element)
+        {
+            Node currNode = _head;
+
+            while (currNode.Value != element || currNode != null)
+            {
+                Console.WriteLine(currNode.Value);
+                currNode = currNode.Link;
+            }
+            _head = _head.Link;
+        }
+
     }
 }
--- Course-Work/Data_Structures/Assignments/Assignment_1/Program_1/Program.cs
+++ Course-Work/Data_Structures/Assignments/Assignment_1/Program_1/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Program_1.Classes;
 
 namespace Program_1
 {
     class Program
     {
+        Stack stack = new Stack();
+
         static void Main(string[] args)
         {
+            int valueCount = 0;
+
+            while (true)
+            {
+                if (Console.Read() == "+")
+                {
+                    
+                }
+                int input = Console.Read();
+
+                if (input == 0 && valueCount == 0)
+                {
+                    Stack.Push(input);
+                }
+            }
         }
+
+
     }
 }

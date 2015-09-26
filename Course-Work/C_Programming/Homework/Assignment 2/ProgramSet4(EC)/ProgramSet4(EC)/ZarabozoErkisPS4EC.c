60fa2ef45a21967daa4ee3529ad4161277bc1e00
Halcyon <vyandra@gmail.com>
2015-09-26 16:41:36 -0700

    Updated ProgramSet4 to VS2015 and added folder for Data_Structures/Labs/Arrays/BinarySearch

---
+++ Course-Work/C_Programming/Homework/Assignment 2/ProgramSet4(EC)/ProgramSet4(EC)/ZarabozoErkisPS4EC.c
@@ -0,0 +1,81 @@
+//==========================================================|
+//					Program Set 4							|
+//----------------------------------------------------------|
+//				   -Description-							|
+//			        Calculator  		    				|
+// Calculator that can add, subtract, divide, and find		|
+// the remainder        									|
+// Special Instructions: Use switch statements				|
+//----------------------------------------------------------|
+// Name:	Erkis Zarabozo									|
+// ID:   	836873											|
+// Class:	CS36											|
+//==========================================================|
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+	void main()
+{
+	double firstNum, secNum, result = 0.00;
+	char operation = 0;
+
+	printf("\nEnter the calculation\n");
+	scanf(" %lf %c %lf", &firstNum, &operation, &secNum);
+
+	switch (operation)
+	{
+	case '+':
+		result = firstNum + secNum;
+		printf("= %lf\n", result);
+		break;
+
+	case '-':
+		result = firstNum - secNum;
+		printf("= %lf\n", result);
+		break;
+
+	case '*':
+		result = firstNum * secNum;
+		printf("= %lf\n", result);
+		break;
+
+	case '/':
+		if (firstNum == 0 || secNum == 0)
+		{
+			printf("Division by zero error!\n");
+			break;
+		}
+
+		result = firstNum / secNum;
+		printf("= %lf\n", result);
+		break;
+
+	case '%':
+		if (firstNum == 0 || secNum == 0)
+		{
+			printf("Division by zero error!\n");
+			break;
+		}
+
+		result = (int)firstNum % (int)secNum;
+		printf("= %d\n", (int)result);
+		break;
+
+	default:
+		printf("Error");
+		break;
+	}
+
+	system("pause");
+}
+
+//======================================================|
+//			       		Output             				|
+//------------------------------------------------------|
+//	Enter the calculation								|
+//	30 * 30												|
+//	= 900.000000										|
+//	Press any key to continue . . .						|
+//======================================================|

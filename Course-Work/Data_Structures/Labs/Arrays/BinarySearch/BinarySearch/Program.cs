9f1ee15b763229d8419c278a08c6e9c2f260e3e4
Halcyon <vyandra@gmail.com>
2015-09-27 14:35:53 -0700

    Formatting

--- Course-Work/Data_Structures/Labs/Arrays/BinarySearch/BinarySearch/Program.cs
+++ Course-Work/Data_Structures/Labs/Arrays/BinarySearch/BinarySearch/Program.cs
@@ -2,7 +2,7 @@
 //				                      Lab	   		                        |
 //--------------------------------------------------------------------------|
 //				                  Description	    	                   	|
-//	   		                     Binary Search	                        |
+//	   		                     Binary Search	                            |
 // Write a function that accepts a sorted array and searches the array      |
 // in a binary fashion, returning the index of the found element,           |
 // or -1 if not found.                                                      |

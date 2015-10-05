cbb8bf69ec87baf076cb17ff8bc45fe6f6df1e7b
unknown <ezarabozo0@IBST110W-44.student.ivc.edu>
2015-10-05 15:06:55 -0700

    Added some of the framework for DS/Program 2

---
+++ DataStructures/Assignments/2_DoubleLinkedList/2_DoubleLinkedList/Classes/LinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_DoubleLinkedList.Classes
+{
+    public class Node
+    {
+        public Node nextName;
+        public Object nameData;
+        public Node nextWeight;
+        public Object weightData;
+    }
+
+    public class LinkedList
+    {
+        private Node nameHead;
+        private Node weightHead;
+
+        public void Add(string Name, int weight)
+        {
+            Node currName = nameHead;
+            Node currWeight = weightHead;
+
+            while (-1 != string.Compare((string)currName.nextName.nameData, Name))
+            {
+
+            }
+
+        }
+        
+        
+        
+        
+        
+        
+        
+        public void printNO()
+        {
+            Node current = nameHead;
+
+            while(current != null)
+            {
+                Console.WriteLine(current.nameData);
+                Console.WriteLine(current.weightData);
+            }
+        }
+
+        public void printWO()
+        {
+            Node current = weightHead;
+
+            while (current != null)
+            {
+                Console.WriteLine(current.nameData);
+                Console.WriteLine(current.weightData);
+            }
+        }
+    }
+    
+}
